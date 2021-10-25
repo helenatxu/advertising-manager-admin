5452cefe62bc1abae20b0d0e7ceeda9952abc6c9
Helena Moreno Chacon <helenaenred@gmail.com>
2021-10-26 00:00:06 +0200

    Imports BrowserRouter and Link from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router} from 'react-router-dom';
 import './App.css';
 import BannersList from './components/BannersList'
 
--- src/components/BannersList.js
+++ src/components/BannersList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import update from 'immutability-helper';
 import axios from 'axios';
 
