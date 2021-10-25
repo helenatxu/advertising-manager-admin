18d6971c2a91b73379cfdde58c2dd1ca8696d410
Helena Moreno Chacon <helenaenred@gmail.com>
2021-10-25 22:03:16 +0200

    Imports the BannersContainer component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import BannersContainer from './components/BannersContainer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="header">
+          <h1>Banner List</h1>
+        </div>
+        <BannersContainer />
+      </div>
+    );
+  }
 }
 
 export default App;
