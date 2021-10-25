9a99179d5f442e9d64adea8dd385271811511c27
Helena Moreno Chacon <helenaenred@gmail.com>
2021-10-25 22:03:01 +0200

    Creates a Banners component plus function to fetch all the banners data

---
+++ src/components/BannersContainer.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+
+class BannersContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      banners: []
+    }
+  }
+
+  getBanners() {
+    fetch('/api/v1/banners')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      this.setState({banners: data})
+    })
+    .catch(error => console.error(error));
+  }
+
+  componentDidMount() {
+    this.getBanners()
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="inputContainer">
+          <input className="bannerInput" type="text"
+            placeholder="Add a banner" maxLength="50"
+            onKeyPress={this.createBanner} />
+        </div>
+  <div className="listWrapper">
+     <ul className="bannerList">
+      {this.state.banners.map((banner) => {
+        return(
+          <li className="banner" banner={banner} key={banner.id}>
+
+      <p className="bannerLabel">{banner.name}</p>
+      <p className="bannerLabel">{banner.text}</p>
+      <span className="deleteBannerBtn">x</span>
+          </li>
+        )
+      })}
+     </ul>
+  </div>
+     </div>
+    )
+  }
+}
+
+export default BannersContainer
