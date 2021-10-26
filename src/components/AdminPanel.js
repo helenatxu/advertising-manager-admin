1cf981f7fa027fcacf8ea7db1cf1a86af36af66c
Helena Moreno Chacon <helenaenred@gmail.com>
2021-10-26 02:46:18 +0200

    Fix component name and reindents code

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -2,15 +2,15 @@ import BannersList from './BannersList';
 import useFetch from './useFetch';
 
 const AdminPanel = () => {
-    const {data: banners, isPending, error} = useFetch('/api/v1/banners');
+  const {data: banners, isPending, error} = useFetch('/api/v1/banners');
 
-    return (
-        <div className="adminPanel">
-            {error && <div>{error}</div>}
-            {isPending && <div>Loading...</div>}
-            <BannerList banners={banners} title="All Banners"/>
-        </div>
-     );
+  return (
+    <div className="adminPanel">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      <BannersList banners={banners} name="All Banners"/>
+    </div>
+  );
 }
 
 export default AdminPanel;
