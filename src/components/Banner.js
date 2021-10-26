b576b122ab964f0ffaa76729bdc2c6c197a9d7e1
Helena Moreno Chacon <helenaenred@gmail.com>
2021-10-26 02:45:12 +0200

    Creates a Banner component to show the detail of a banner

---
+++ src/components/Banner.js
@@ -0,0 +1,31 @@
+import { useParams, useHistory } from 'react-router-dom';
+import useFetch from './useFetch';
+
+const Banner = () => {
+  const { id } = useParams();
+  const { data: banner, error, isPending } =  useFetch('/api/v1/banners/' + id);
+  const history = useHistory();
+
+  const handleClick = () => {
+    fetch('/api/v1/banners'+ banner.id, {
+      method: 'DELETE'
+    }).then(() => {
+      history.push('/');
+    })
+  }
+
+  return (
+    <div className="banner-details">
+      <h1>Banner Details</h1>
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      <article >
+        <p>{banner.name}</p>
+        <div>{banner.text}</div>
+        <button onClick={handleClick}>Delete</button>
+      </article>
+    </div>
+  );
+}
+
+export default Banner;
