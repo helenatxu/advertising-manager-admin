c543fedda5c0d887c34698bd454559f117cf965c
Helena Moreno Chacon <helenaenred@gmail.com>
2021-10-26 04:54:54 +0200

    Adds link to campaigns and updates the path in the navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,7 +6,8 @@ const Navigation = () => {
       <h1>React Admin App</h1>
       <div className="links">
         <Link to="/">Admin main page</Link>
-        <Link to="/create">Create Banner</Link>
+        <Link to="/create_banner">Create Banner</Link>
+        <Link to="/create_campaign">Create Campaign</Link>
       </div>
     </nav>
   );
