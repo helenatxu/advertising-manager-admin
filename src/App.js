ba082037a5b20cb22715aed0115deadf1de60f8d
Helena Moreno Chacon <helenaenred@gmail.com>
2021-10-26 03:04:36 +0200

    Imports new components and adds them to the template

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
-import BannersList from './components/BannersList'
+import AdminPanel from './components/AdminPanel'
+import Navigation from './components/Navigation';
+import Create from './components/Create';
+import Banner from './components/Banner';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
+          <Navigation/>
           <div className="list">
             <div className="header">
-              <h1>Banner List</h1>
+              <h1>Banners List</h1>
             </div>
-            <BannersList />
+            <Switch>
+            <Route exact path="/">
+              <AdminPanel/>
+            </Route>
+            <Route path="/create">
+              <Create/>
+            </Route>
+            <Route path="/api/v1/banners/:id">
+              <Banner/>
+            </Route>
+          </Switch>
           </div>
         </div>
       </Router>
