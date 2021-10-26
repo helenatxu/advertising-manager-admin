05be04823e8f7b973bbc65825f11acd34d3d8cd7
Helena Moreno Chacon <helenaenred@gmail.com>
2021-10-26 04:55:41 +0200

    Adds link to campaigns and updates the path in the app component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 import AdminPanel from './components/AdminPanel'
 import Navigation from './components/Navigation';
-import Create from './components/Create';
+import CreateBanner from './components/CreateBanner';
+import CreateCampaign from './components/CreateCampaign';
 import Banner from './components/Banner';
 import BannersList from './components/BannersList';
+import Campaign from './components/Campaign';
+import CampaignsList from './components/CampaignsList';
 
 class App extends Component {
   render() {
@@ -20,14 +23,23 @@ class App extends Component {
             <Route exact path="/">
               <AdminPanel/>
             </Route>
-            <Route path="/create">
-              <Create/>
+            <Route path="/create_banner">
+              <CreateBanner/>
+            </Route>
+            <Route path="/create_campaign">
+              <CreateCampaign/>
+            </Route>
+            <Route path="/api/v1/banners">
+              <BannersList/>
             </Route>
             <Route path="/api/v1/banners/:id">
               <Banner/>
             </Route>
-            <Route path="/api/v1/banners">
-              <BannersList/>
+            <Route path="/api/v1/campaigns">
+              <CampaignsList/>
+            </Route>
+            <Route path="/api/v1/campaigns/:id">
+              <Campaign/>
             </Route>
           </Switch>
           </div>
